d3aec17801937c34155ccf5011a955f23c2beebe
Zachary Goodman <znote89@gmail.com>
2021-06-09 13:18:46 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert); 
+}
+
+addingEventListener();
